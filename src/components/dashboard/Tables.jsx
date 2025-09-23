e6494006ac0539f3d2d59d67a9f81c39383a7dac
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-09-23 13:05:58 +0530

    update: added the react select library and used the select tag

--- src/components/dashboard/Tables.jsx
+++ src/components/dashboard/Tables.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Select from 'react-select';
+
+const options = [
+    { value: 1, label: 'Sushil Kumar Jha' },
+    { value: 2, label: 'Amit Kumar' },
+];
+
 
 const Tables = () => {
+
+    const [selectedOption, setSelectedOption] = useState(null);
     return (
-        <div className='bg-white rounded-lg p-4 bottom-cell'>
-            <h1>My Tables</h1>
+        <div className='bg-white rounded-lg p-4 flex flex-col gap-4 bottom-cell'>
+            <div className="input-fields flex flex-col items-start gap-2">
+                <label htmlFor='WM-Select'>Assign Agent</label>
+                <div className='w-[220px]'>
+                    <Select
+                        defaultValue={"Select Agent"}
+                        onChange={setSelectedOption}
+                        options={options}
+                        name="WM-Select"
+                    />
+                </div>
+            </div>
+            <div className="tables">
+                <p>
+                    {selectedOption?.value}
+                </p>
+            </div>
         </div>
     )
 }
 
-export default Tables
+export default Tables;
