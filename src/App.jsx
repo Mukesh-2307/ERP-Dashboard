f4d84a99c3056c6772245d7fef700abb06ce3ba0
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-09-23 14:35:36 +0530

    update: removed unwanted codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import InputFields from './components/dashboard/InputFields'
 import Tables from './components/dashboard/Tables'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='grid gap-4 p-4 grid-row-[220px_1fr]'>
