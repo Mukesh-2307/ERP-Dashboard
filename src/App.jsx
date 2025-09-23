8d512f79f473a5645a4ed13ce88fd1d6bf8b7daa
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-09-23 12:33:32 +0530

    update: built base layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import InputFields from './components/dashboard/InputFields'
+import Tables from './components/dashboard/Tables'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className='grid gap-4 p-4 grid-row-[220px_1fr]'>
+      <InputFields />
+      <Tables />
+    </div>
   )
 }
 
