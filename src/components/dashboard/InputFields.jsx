81d4c6ff005eee9d40f53e25ceb477f7cbf3d438
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-09-23 12:33:45 +0530

    update: added two new components

---
+++ src/components/dashboard/InputFields.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputFields = () => {
+    return (
+        <div className='bg-white rounded-lg p-4 h-[220px]'>
+            <h1>Input Fields</h1>
+        </div>
+    )
+}
+
+export default InputFields
---
+++ src/components/dashboard/Tables.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tables = () => {
+    return (
+        <div className='bg-white rounded-lg p-4 bottom-cell'>
+            <h1>My Tables</h1>
+        </div>
+    )
+}
+
+export default Tables
