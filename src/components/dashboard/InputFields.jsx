1dfa26ab52fc789d944f7f841f17c832a558a4f4
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-09-23 13:04:51 +0530

    update: imported used the stats card component

--- src/components/dashboard/InputFields.jsx
+++ src/components/dashboard/InputFields.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import StatsCard from '../StatsCard'
 
 const InputFields = () => {
     return (
-        <div className='bg-white rounded-lg p-4 h-[220px]'>
-            <h1>Input Fields</h1>
+        <div className='rounded-lg h-[220px] grid gap-4 grid-cols-3'>
+            <StatsCard />
         </div>
     )
 }
