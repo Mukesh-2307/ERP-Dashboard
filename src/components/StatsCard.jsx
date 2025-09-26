43e85c0e9a0793c4518b9be1bf6303d5a862aa8a
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-09-26 12:08:57 +0530

    update: add classes to their respective divs.

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const StatsCard = () => {
     return (
         <>
-            <Card title="Total Entries" value={100} />
-            <Card title="Total Deliveries" value={44} />
-            <Card title="Total Pickups" value={56} />
+            <Card title="Total Entries" value={100} className="card-dark" />
+            <Card title="Total Deliveries" value={44} className="card-light" />
+            <Card title="Total Pickups" value={56} className="card-light" />
         </>
     )
 }
 
-const Card = ({ title, value }) => {
+const Card = ({ title, value, className }) => {
     return (
-        <div className='bg-white text-black rounded-lg p-4'>
-            <p>{title}</p>
-            <h1>{value}</h1>
+        <div className={`rounded-lg p-4 shadow flex flex-col justify-between ${className}`}>
+            <p className='font-semibold'>{title}</p>
+            <h1 className='card-text-l py-5'>{value}</h1>
         </div>
     )
 }
