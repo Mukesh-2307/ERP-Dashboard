a484cc3b4e23ecb7e1926893d67a94367ad85eb0
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-09-29 17:43:13 +0530

    update: added the logic for calculating total entries, total deliveries and total pickups

---
+++ src/components/dashboard/StatCards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import StatsCard from '../StatsCard'
+
+const StatCards = ({ deliveryData, pickupData, manager }) => {
+    const totalDeliveries = manager ? deliveryData.filter((row) => row.WM_ID === manager.value) : 0;
+    const totalPickups = manager ? pickupData.filter((row) => row.WM_ID === manager.value) : 0;
+    const total = totalDeliveries.length + totalPickups.length;
+
+    return (
+        <div className='rounded-lg h-[220px] grid gap-4 grid-cols-3'>
+            <StatsCard total={total || 0} totalDeliveries={totalDeliveries.length || 0} totalPickups={totalPickups.length || 0} />
+        </div>
+    )
+}
+
+export default StatCards
