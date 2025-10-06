f1e098d7dc25b220959936e570de8b9e76a20b58
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-10-06 18:46:03 +0530

    refactor: updated the code for adding react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import './index.css'
 import App from './App.jsx'
+import MainDashboard from './pages/MainDashboard.jsx';
+import WishMaster from './pages/WishMaster.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainDashboard />,
+  },
+  {
+    path: "/WM-Dashboard",
+    element: <WishMaster />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />,
+);
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
