1deda4e83b06131c5cfb1492e38e8dc34643587b
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-10-06 13:25:57 +0530

    update: receiving values as integer instead of array, so removed .length method

--- src/components/dashboard/StatCards.jsx
+++ src/components/dashboard/StatCards.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import StatsCard from '../StatsCard'
 
-const StatCards = ({ deliveryData, pickupData, manager }) => {
-    const totalDeliveries = manager ? deliveryData.filter((row) => row.WM_ID === manager.value) : 0;
-    const totalPickups = manager ? pickupData.filter((row) => row.WM_ID === manager.value) : 0;
-    const total = totalDeliveries.length + totalPickups.length;
+const StatCards = ({isPopulate, total, totalDeliveries, totalPickups }) => {
 
     return (
         <div className='rounded-lg h-[150px] grid gap-4 grid-cols-3'>
-            <StatsCard total={total || 0} totalDeliveries={totalDeliveries.length || 0} totalPickups={totalPickups.length || 0} />
+            <StatsCard total={total || 0} totalDeliveries={totalDeliveries || 0} totalPickups={totalPickups || 0} isPopulate={isPopulate} />
         </div>
     )
 }
